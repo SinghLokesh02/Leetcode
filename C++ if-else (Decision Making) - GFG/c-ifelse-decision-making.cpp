8efc905d4700018cbc14b661f66292e053090d0a
Lokesh Singh <130292787+SinghLokesh02@users.noreply.github.com>
2023-04-16 17:20:57 +0530

    Added solution - LeetHub

---
+++ C++ if-else (Decision Making) - GFG/c-ifelse-decision-making.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string compareFive(int n){
+        // code here 
+        if(n==5)return "Equal to 5";
+        return n<5?"Less than 5":"Greater than 5";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.compareFive(N) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
