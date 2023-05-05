00c89ac466c5f490aa43953875dd37717d35f5cb
Lokesh Singh <130292787+SinghLokesh02@users.noreply.github.com>
2023-05-05 14:24:37 +0530

    Added Solution - GfG to GitHub

---
+++ School/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string revStr(string S) {
+       string s1;
+       for(int i = S.size()-1;i>=0;i--){
+           s1 += S[i];
+       }
+        return s1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.revStr(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
