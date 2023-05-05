abfb1ec02678fda2b9022292eb3f276e3d777e94
Lokesh Singh <130292787+SinghLokesh02@users.noreply.github.com>
2023-05-05 14:12:58 +0530

    Added Solution - GfG to GitHub

---
+++ School/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        // Method-1
+        string new_s;
+        for(int i = 0;i<s.size();i++){
+            if(s[i]!=' '){
+                new_s += s[i];
+            }
+        }
+        return new_s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
