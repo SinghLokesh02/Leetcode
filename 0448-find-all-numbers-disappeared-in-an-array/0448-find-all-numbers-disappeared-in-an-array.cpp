a26a2cf9829888468e052f425d88ed3187229b00
Lokesh Singh <130292787+SinghLokesh02@users.noreply.github.com>
2023-10-06 13:46:32 +0530

    Time: 36 ms (88.27%), Space: 34 MB (64.20%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,12 +1,45 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-         vector<int>ans;
+        
+        // brute Force Using Map (Time -> 0(n), space -> 0(n))
+        /*
+        
+        vector<int>ans;
         unordered_map<int,int>m;
         for(auto i : nums)m[i]++;
         for(int i = 0;i<nums.size();i++){
             if(m.find(i+1)==m.end())ans.push_back(i+1);
         }
+        return ans;
+        
+        */
+        
+        // Method -2 By Using Frequency Array
+        /*
+        
+        int n = nums.size();
+        vector<int>ans;
+        vector<bool>extra(n,0);
+        for(int i = 0;i<n;i++){
+            extra[nums[i]] = 1;
+        }
+        
+        for(int i = 1;i<=n;i++)
+            if(extra[i] == 0)ans.push_back(i);
+        return ans;
+        
+        */
+        
+        // Method - 3 Negate The Indices that are present
+        vector<int>ans;
+       for(auto i = 0;i<nums.size();i++)
+           if(nums[abs(nums[i])-1]>0)nums[abs(nums[i])-1] *= -1;
+       
+        
+        for(int i = 0;i<nums.size();i++)
+            if(nums[i]>0)ans.push_back(i+1);
+        
         return ans;
     }
 };
