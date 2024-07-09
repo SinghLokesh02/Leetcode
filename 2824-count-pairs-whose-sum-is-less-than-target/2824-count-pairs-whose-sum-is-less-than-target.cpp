f092ccaa526072c53ad8467e216a38d7c1ae7093
Lokesh Singh <130292787+SinghLokesh02@users.noreply.github.com>
2024-07-09 09:12:59 +0530

    Time: 8 ms (25.41%), Space: 23.3 MB (30.36%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -3,24 +3,24 @@ public:
     int countPairs(vector<int>& nums, int target) {
 // APPROACH - 1
         int pair = 0, n = nums.size();
-        // for(int i = 0;i<n;i++){
-        //     for(int j = i+1;j<n;j++){
-        //         if(nums[i]+nums[j] < target)pair++;
-        //     }
-        // }
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(nums[i]+nums[j] < target)pair++;
+            }
+        }
         
 // APPROACH - 2
-        sort(nums.begin(),nums.end());
-        int i = 0, j = n-1;
-        while(i<j){
-            int sum = nums[i]+nums[j];
-            if(sum < target){
-                pair += j - i;
-                i++;
-            }
+//         sort(nums.begin(),nums.end());
+//         int i = 0, j = n-1;
+//         while(i<j){
+//             int sum = nums[i]+nums[j];
+//             if(sum < target){
+//                 pair += j - i;
+//                 i++;
+//             }
             
-            else j--;
-        }
+//             else j--;
+//         }
         return pair;
     }
 };
